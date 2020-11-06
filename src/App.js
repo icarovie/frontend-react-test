3805fd501ef483269f9b613ca6880d76536327b2
ïcarovieira hotmart <carovieira@hotmart.com>
2020-11-06 14:37:29 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo.png" />
+        <p>
+          Aplicativo de Leitura
+        </p>
+        <a
+          className="App-link"
+          href="logo.png"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
